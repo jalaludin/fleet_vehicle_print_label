29979816105d020dcc40be3bde9f832150186d23
stellaf <stella.fredo@gmail.com>
2017-02-02 00:58:37 +0100

    Add files via upload

---
+++ fleet_vehicle_print/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Fleet Vehicle Print Barcode label',
+    'version': '10.0.1.0',
+    'category': 'Managing vehicles and contracts',
+    'sequence': 110,
+    'summary': "Print barcode label for fleet vehicle",
+    'description':"Print barcode label for fleet vehicle, this works for v10, v9, v8",
+    'author': 'Stella Fredö in Sweden',
+    'website': 'http://se.linkedin.com/in/stella-fredö-94401014',
+    'depends': ['fleet'],
+    'data': [
+        'views/fleet_report.xml',
+        'fleet_vehicle_report.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+
