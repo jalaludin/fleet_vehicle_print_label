3af8e45e6be26bc3820e5b2344f266cf45dc87a6
stellaf <stella.fredo@gmail.com>
2017-02-02 01:25:04 +0100

    Update __openerp__.py

--- fleet_vehicle_print/__openerp__.py
+++ fleet_vehicle_print/__openerp__.py
@@ -4,7 +4,7 @@
     'category': 'Managing vehicles and contracts',
     'sequence': 110,
     'summary': "Print barcode label for fleet vehicle",
-    'description':"Print barcode label for fleet vehicle, this works for v10, v9, v8. You find the print button beside Action button on the top of fleet vehicle form.",
+    'description':"Print barcode label for fleet vehicle, this works for v10, v9, v8. You find the print button beside Action button on the top of fleet vehicle form. In the system parameter, you shall add key report.url  and value is http://localhost:8069, key webkit.path and value is http://localhost:8069",
     'author': 'Stella Fredö in Sweden',
     'website': 'http://se.linkedin.com/in/stella-fredö-94401014',
     'depends': ['fleet'],
